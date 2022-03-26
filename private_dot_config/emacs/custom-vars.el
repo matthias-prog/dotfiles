4569f802554f3542d654d5e31de134a59c43b214
matthias <matthias.gelszinnis@gmail.com>
2022-03-26 16:08:21 +0100

    add emacs custom-vars.el

---
+++ private_dot_config/emacs/custom-vars.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
+ '(ivy-mode t)
+ '(package-selected-packages
+   '(yaml-mode ivy-prescient all-the-icons-ivy-rich evil-nerd-commenter evil-magit magit counsel-projectile projectile hydra general evil-collection evil all-the-icons-dired emojify lua-mode slime helpful counsel ivy-rich which-key rainbow-delimiters doom-themes doom-modeline ivy use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
